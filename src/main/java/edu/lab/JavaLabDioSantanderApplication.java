6ff144980ecaabd99674dad57b70b9eccce1728c
wsbittencourt <wagner.guio@gmail.com>
2023-09-17 18:29:18 -0300

    Initial project architecture

---
+++ src/main/java/edu/lab/JavaLabDioSantanderApplication.java
@@ -0,0 +1,13 @@
+package edu.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLabDioSantanderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLabDioSantanderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/lab/JavaLabDioSantanderApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLabDioSantanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
